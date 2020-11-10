3410fdbff5a35251f52e8adb846650ebc74091b6
Kshitij Sobti <kshitij@opencraft.com>
2020-11-10 10:05:40 +0000

    Update src/components/value-or-loading/ValueOrLoading.jsx
    Co-authored-by: Giovanni Cimolin da Silva <giovannicimolin@gmail.com>

--- src/components/value-or-loading/ValueOrLoading.jsx
+++ src/components/value-or-loading/ValueOrLoading.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import PropType from 'prop-types';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-// import { faSpi } from '@fortawesome/free-regular-svg-icons';
 import { faSpinner } from '@fortawesome/free-solid-svg-icons';
 
 function LoadingOrValue({ value }) {
